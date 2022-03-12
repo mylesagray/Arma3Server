e10628f25e054e671b989611a3aa856019b73b75
minuq <linke.florian@gmail.com>
2022-03-12 15:45:48 +0100

    pull request

--- update.py
+++ update.py
@@ -36,23 +36,7 @@ def env_defined(key):
 
 
 # get install state
-# valid returns: "uninstalled", "Fully Installed", "no idea since there's no documentation"
-#  - release state: released (No License)
-#  - install state: Update Required,Update Started,
-#  - install dir: "/arma3"
-#  - mounted depots:
-#  - size on disk: 0 bytes, BuildID 0
-#  - update started: Thu Jan  1 00:00:00 1970, staged: 6914/6914 MB 100%, downloaded: 2775/2775 MB 100% - 0 KB/s
-#  - update state:  ( No Error )
-#  - user config: "UserConfig"
-# root@docker-desktop:/arma3# cat startup.log
-# 11:19:02 Dedicated host created.
-# 11:19:16 BattlEye server updated to version: 217
-# 11:19:16 BattlEye Server: Initialized (v1.217)
-# 11:19:16 Game Port: 2302, Steam Query Port: 2303
-# Arma 3 Console version 2.08.148892 x64 : port 2302
-# 11:19:16 Host identity created.
-
+# valid returns: "uninstalled", "Fully Installed", "Update Required,Update Started," "no idea since there's no documentation"
 
 def get_install_state():
   install_state = "undefined"
